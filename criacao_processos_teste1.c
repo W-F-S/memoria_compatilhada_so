2fe91845d6d104861a86e0eff91db3ece5b10310
W-F-S <walkerfreitassantos@gmail.com>
2024-09-26 07:10:30 -0300

    .

--- criacao_processos_teste1.c
+++ criacao_processos_teste1.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>   // Para fork()
-#include <sys/wait.h> // Para wait()
+#include <unistd.h>   
+#include <sys/wait.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-char* memoria_compartilhada(int tam){
-  key_t keytmp = ftok("memoria", 123);
+
+int memSize = 512; 
+
+int memoria_compartilhada(int tam){
+  key_t keytmp = ftok("./pid.txt", 123);
   int shmid = shmget(keytmp, tam, 0666 | IPC_CREAT);
-  return (char*)shmat(shmid, (void*)0, 0);
+  
+  return (shmid);
 }
 
 int main(int argc, char *argv[]){
   pid_t pid;
   int count = 0;
-  int limit = atoi(argv[1]);
+  int memId = 0;
+  void* data = NULL;
+  char buffer[11];
 
+  //int limit = atoi(argv[1]); //limite de processos criados
+  memId = memoria_compartilhada(memSize);
+
+  printf("Memory attached at %p\n", memId);
+  data = shmat( memId, (void *)0, 0);
+  printf("Enter something: ");
+  fgets(buffer, 11, stdin)
+  strncpy(shared)
+  strncpy(buffer, 11, stdin);
+
+  printf("Memory attached at %p\n", data);
+
+
+
+
+
+  /*
   if(limit <= 0){
     limit = 2;
   }
 
   while (count < limit)
+ 
   {
     pid = fork();
     printf("\ncount, %d\n", count);
@@ -30,6 +56,12 @@ int main(int argc, char *argv[]){
       printf("Esse é o pai %d", getpid());
     }
     count++;
+  }
+  */
+
+  while(1){
+    printf("aguardando\n");
+    sleep(20);
   }
 
   printf("\nfim do programa.\n\n");
