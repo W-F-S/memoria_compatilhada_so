a7eaa607e8a46a84acd392e315b56eb2f1121d7a
W-F-S <walkerfreitassantos@gmail.com>
2024-09-26 23:27:08 -0300

    .

--- criacao_processos_teste1.c
+++ criacao_processos_teste1.c
@@ -23,9 +23,18 @@ int memoria_compartilhada(){
   return (shmid);
 }
 
+int ler_mem(){
+
+}
+
+int escrever_mem(){
+
+}
+
 int main(int argc, char *argv[]){
   pid_t pid;
   int memId = 0;
+  int qt_processos =4;
   int rd_temp = 0;
   void* data = NULL;
   struct campo_compartilhado* memoria;
@@ -41,15 +50,54 @@ int main(int argc, char *argv[]){
   }
 
   memoria = (struct campo_compartilhado*) data;
-
   memoria->flag_semaforo = 0;
 
-
-  printf("Pai, valor semaforo: %d", memoria->flag_semaforo);
-
-
+  //processo consumidor
   pid = fork();
+  if(pid == 0){
+    while(1){
+        if(memoria->flag_semaforo == 0){
+          printf("\nlendo\n");
+          memoria->flag_semaforo = 1;
+          sleep(1);
+          memoria->flag_semaforo = 0;
+        }else{
+          sleep(rand() % (5));
+        }
+    }
+  }else if(pid < 0 ){
+    printf("\nErro ao criar processo leitor\n");
+  }else{
+    for(int i = 0; i < qt_processos; i++){
+      pid = fork();
+      if (pid < 0) {
+        printf("\nErro ao criar processo\n");
+      } else if (pid == 0) {
+          while(1){
+            if(memoria->flag_semaforo == 0){
+              printf("\nEscrevendo");
+              memoria->flag_semaforo = 1;
+              int rd_pos = rand() % (DATA_SZ - 1); //selecionando posicao aleatoria
+              int rd_num = rand() % (100 - 1 + 1); //criando valor aleatorio
 
+              printf("Escrendo %d, na posicao %d\n", rd_num, rd_pos);
+
+              memoria->dados[rd_pos] = rd_num;
+              sleep(1);
+              memoria->flag_semaforo = 0;
+            }else{
+              sleep(rand() % (5));
+            }
+          }
+      }else{
+        //pai
+      }
+    }
+
+  }
+
+
+/*
   if(pid==0){
     while(1){
 
@@ -101,6 +149,7 @@ int main(int argc, char *argv[]){
 
     }
   }
+  */
 
   printf("\nfim do programa.\n\n");
 }
